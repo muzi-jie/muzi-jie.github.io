80a0c4c73006800484b43456c93378120afd8e6d
jie <100739160+2936170194@users.noreply.github.com>
2026-01-11 03:10:11 +0800

    Update social links in constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -19,25 +19,25 @@ interface Social {
 export const SOCIALS: Social[] = [
   {
     name: "GitHub",
-    href: "https://github.com/satnaing/astro-paper",
+    href: "https://github.com/2936170194/2936170194.github.io",
     linkTitle: `${SITE.title} on GitHub`,
     icon: IconGitHub,
   },
   {
     name: "X",
-    href: "https://x.com/username",
+    href: "",
     linkTitle: `${SITE.title} on X`,
     icon: IconBrandX,
   },
   {
     name: "LinkedIn",
-    href: "https://www.linkedin.com/in/username/",
+    href: "",
     linkTitle: `${SITE.title} on LinkedIn`,
     icon: IconLinkedin,
   },
   {
     name: "Mail",
-    href: "mailto:yourmail@gmail.com",
+    href: "mailto:2936170194@qq.com",
     linkTitle: `Send an email to ${SITE.title}`,
     icon: IconMail,
   },
@@ -46,37 +46,37 @@ export const SOCIALS: Social[] = [
 export const SHARE_LINKS: Social[] = [
   {
     name: "WhatsApp",
-    href: "https://wa.me/?text=",
+    href: "",
     linkTitle: `Share this post via WhatsApp`,
     icon: IconWhatsapp,
   },
   {
     name: "Facebook",
-    href: "https://www.facebook.com/sharer.php?u=",
+    href: "",
     linkTitle: `Share this post on Facebook`,
     icon: IconFacebook,
   },
   {
     name: "X",
-    href: "https://x.com/intent/post?url=",
+    href: "",
     linkTitle: `Share this post on X`,
     icon: IconBrandX,
   },
   {
     name: "Telegram",
-    href: "https://t.me/share/url?url=",
+    href: "",
     linkTitle: `Share this post via Telegram`,
     icon: IconTelegram,
   },
   {
     name: "Pinterest",
-    href: "https://pinterest.com/pin/create/button/?url=",
+    href: "",
     linkTitle: `Share this post on Pinterest`,
     icon: IconPinterest,
   },
   {
     name: "Mail",
-    href: "mailto:?subject=See%20this%20post&body=",
+    href: "",
     linkTitle: `Share this post via email`,
     icon: IconMail,
   },
